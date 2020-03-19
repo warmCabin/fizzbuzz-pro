e8720fa0eb9b9eea17ba0272dff0df2abd4ee70e
warmCabin <35278529+warmCabin@users.noreply.github.com>
2020-03-18 22:50:21 -0400

    refactor: initialize checkers in the constructor
    Previously, checkers were initialized in the generate method.
    Since these values don't change, this work was causing unnecesasry
    CPU strain.

--- src/buzz/fizz/generator/StandardFizzBuzzGenerator.java
+++ src/buzz/fizz/generator/StandardFizzBuzzGenerator.java
@@ -3,27 +3,37 @@ package buzz.fizz.generator;
 import buzz.fizz.checker.AndChecker;
 import buzz.fizz.checker.Checker;
 import buzz.fizz.checker.IntegerDivisibilityChecker;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StandardFizzBuzzGenerator implements Generator {
 
   private static final Logger log = Logger.getLogger(StandardFizzBuzzGenerator.class.getName());
 
-  @Override
-  public void generate(int lower, int upper) {
-    Checker fizzChecker = new IntegerDivisibilityChecker(3, "Fizz");
-    Checker buzzChecker = new IntegerDivisibilityChecker(5, "Buzz");
-    Checker fizzBuzzChecker = new AndChecker(fizzChecker, buzzChecker);
+  private final List<Checker> checkers;
 
-    for(int i = lower; i <= upper; i++) {
-      if (fizzBuzzChecker.isValid(i)) {
-        log.info(fizzBuzzChecker.getReplacement());
-      } else if (fizzChecker.isValid(i)) {
-        log.info(fizzChecker.getReplacement());
-      } else if (buzzChecker.isValid(i)) {
-        log.info(buzzChecker.getReplacement());
-      } else {
-        log.info(Integer.toString(i));
+  public StandardFizzBuzzGenerator() {
+    final Checker fizzChecker = new IntegerDivisibilityChecker(3, "Fizz");
+    final Checker buzzChecker = new IntegerDivisibilityChecker(5, "Buzz");
+    final Checker fizzBuzzChecker = new AndChecker(fizzChecker, buzzChecker);
+
+    checkers = Arrays.asList(fizzBuzzChecker, fizzChecker, buzzChecker);
+  }
+
+  @Override
+  public void generate(final int lower, final int upper) {
+
+    for (int i = lower; i <= upper; i++) {
+      final int val = i;
+      if (log.isLoggable(Level.INFO)) {
+        log.info(checkers.stream()
+            .filter(checker -> checker.isValid(val))
+            .findFirst()
+            .map(Checker::getReplacement)
+            .orElse(Integer.toString(i))
+        );
       }
     }
   }
